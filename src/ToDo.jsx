e89f68796423fd9af878efe1ac30883059fc254d
jennyvanessaquinto <vane.quinto19@gmail.com>
2022-11-07 01:11:25 -0500

    Setting grid

--- src/ToDo.jsx
+++ src/ToDo.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
+import './index.scss';
 import Header from './toDo/header/Header';
+
 
 const ToDo = () => {
     return (
         <>
             <Header />
+            <div className="main">
+                <div className="taskList">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, id necessitatibus! Dolore natus, eius modi hic sunt quis nostrum quidem distinctio inventore ducimus ex nemo, qui consequatur eos molestias aperiam.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, id necessitatibus! Dolore natus, eius modi hic sunt quis nostrum quidem distinctio inventore ducimus ex nemo, qui consequatur eos molestias aperiam.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, id necessitatibus! Dolore natus, eius modi hic sunt quis nostrum quidem distinctio inventore ducimus ex nemo, qui consequatur eos molestias aperiam.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, id necessitatibus! Dolore natus, eius modi hic sunt quis nostrum quidem distinctio inventore ducimus ex nemo, qui consequatur eos molestias aperiam.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, id necessitatibus! Dolore natus, eius modi hic sunt quis nostrum quidem distinctio inventore ducimus ex nemo, qui consequatur eos molestias aperiam.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, id necessitatibus! Dolore natus, eius modi hic sunt quis nostrum quidem distinctio inventore ducimus ex nemo, qui consequatur eos molestias aperiam.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, id necessitatibus! Dolore natus, eius modi hic sunt quis nostrum quidem distinctio inventore ducimus ex nemo, qui consequatur eos molestias aperiam.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, id necessitatibus! Dolore natus, eius modi hic sunt quis nostrum quidem distinctio inventore ducimus ex nemo, qui consequatur eos molestias aperiam.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, id necessitatibus! Dolore natus, eius modi hic sunt quis nostrum quidem distinctio inventore ducimus ex nemo, qui consequatur eos molestias aperiam.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, id necessitatibus! Dolore natus, eius modi hic sunt quis nostrum quidem distinctio inventore ducimus ex nemo, qui consequatur eos molestias aperiam.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, id necessitatibus! Dolore natus, eius modi hic sunt quis nostrum quidem distinctio inventore ducimus ex nemo, qui consequatur eos molestias aperiam.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, id necessitatibus! Dolore natus, eius modi hic sunt quis nostrum quidem distinctio inventore ducimus ex nemo, qui consequatur eos molestias aperiam.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, id necessitatibus! Dolore natus, eius modi hic sunt quis nostrum quidem distinctio inventore ducimus ex nemo, qui consequatur eos molestias aperiam.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, id necessitatibus! Dolore natus, eius modi hic sunt quis nostrum quidem distinctio inventore ducimus ex nemo, qui consequatur eos molestias aperiam.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, id necessitatibus! Dolore natus, eius modi hic sunt quis nostrum quidem distinctio inventore ducimus ex nemo, qui consequatur eos molestias aperiam.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, id necessitatibus! Dolore natus, eius modi hic sunt quis nostrum quidem distinctio inventore ducimus ex nemo, qui consequatur eos molestias aperiam.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, id necessitatibus! Dolore natus, eius modi hic sunt quis nostrum quidem distinctio inventore ducimus ex nemo, qui consequatur eos molestias aperiam.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, id necessitatibus! Dolore natus, eius modi hic sunt quis nostrum quidem distinctio inventore ducimus ex nemo, qui consequatur eos molestias aperiam.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, id necessitatibus! Dolore natus, eius modi hic sunt quis nostrum quidem distinctio inventore ducimus ex nemo, qui consequatur eos molestias aperiam.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, id necessitatibus! Dolore natus, eius modi hic sunt quis nostrum quidem distinctio inventore ducimus ex nemo, qui consequatur eos molestias aperiam.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, id necessitatibus! Dolore natus, eius modi hic sunt quis nostrum quidem distinctio inventore ducimus ex nemo, qui consequatur eos molestias aperiam.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, id necessitatibus! Dolore natus, eius modi hic sunt quis nostrum quidem distinctio inventore ducimus ex nemo, qui consequatur eos molestias aperiam.</p>
+                </div>
+                <div className="form">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, id necessitatibus! Dolore natus, eius modi hic sunt quis nostrum quidem distinctio inventore ducimus ex nemo, qui consequatur eos molestias aperiam.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, id necessitatibus! Dolore natus, eius modi hic sunt quis nostrum quidem distinctio inventore ducimus ex nemo, qui consequatur eos molestias aperiam.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, id necessitatibus! Dolore natus, eius modi hic sunt quis nostrum quidem distinctio inventore ducimus ex nemo, qui consequatur eos molestias aperiam.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, id necessitatibus! Dolore natus, eius modi hic sunt quis nostrum quidem distinctio inventore ducimus ex nemo, qui consequatur eos molestias aperiam.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, id necessitatibus! Dolore natus, eius modi hic sunt quis nostrum quidem distinctio inventore ducimus ex nemo, qui consequatur eos molestias aperiam.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, id necessitatibus! Dolore natus, eius modi hic sunt quis nostrum quidem distinctio inventore ducimus ex nemo, qui consequatur eos molestias aperiam.</p>
+                    
+                </div>
+            </div>
         </>
     )
 };
