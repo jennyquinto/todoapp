c6e3724987bcd356d42bc0cf807626056e968baa
jennyvanessaquinto <vane.quinto19@gmail.com>
2022-11-06 23:45:26 -0500

    Setting branchs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-// import App from './App';
-import ToDo from './ToDo';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( 
-  <ToDo />
+root.render(
+  <App />
 );
